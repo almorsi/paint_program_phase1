e87a97412d20f52a70c3b95977891538cc354cee
Ammar Nasser <ammarnasser0012@gmail.com>
2021-04-29 17:48:22 +0200

    edit a typo in getString and virtual key

--- GUI/Input.cpp
+++ GUI/Input.cpp
@@ -12,18 +12,18 @@ void Input::GetPointClicked(int &x, int &y) const
 	pWind->WaitMouseClick(x, y);	//Wait for mouse click
 }
 
-string Input::GetSrting(Output *pO) const 
+string Input::GetString(Output *pO) const 
 {
 	string Label;
 	char Key;
 	while(1)
 	{
 		pWind->WaitKeyPress(Key);
-		if(Key == 27 )	//ESCAPE key is pressed
+		if(Key == VK_ESCAPE)	//ESCAPE key is pressed
 			return "";	//returns nothing as user has cancelled label
-		if(Key == 13 )	//ENTER key is pressed
+		if(Key == VK_RETURN )	//ENTER key is pressed
 			return Label;
-		if(Key == 8 )	//BackSpace is pressed
+		if(Key == VK_BACK )	//BackSpace is pressed
 			Label.resize(Label.size() -1 );			
 		else
 			Label+= Key;
