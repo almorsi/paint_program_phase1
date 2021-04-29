6fac9ff0bf1c347f9729d741c342031f5aa85a16
Ammar Nasser <ammarnasser0012@gmail.com>
2021-04-29 17:31:10 +0200

    add a temp constant to playMenuItem to avoid an error when creating an array of menu item

--- GUI/UI_Info.h
+++ GUI/UI_Info.h
@@ -34,7 +34,7 @@ enum PlayMenuItem //The items of the Play menu (you should add more items)
 	//If you want to change the menu items order, change the order here
 	
 	//TODO: Add more items names here
-
+	temp, // just for now to eleminate the error
 	PLAY_ITM_COUNT		//no. of menu items ==> This should be the last line in this enum
 	
 };
